258e0ffb0f754af760367f14ce058997b0ece237
AMNOON SECURITY CONSULTING <161709032+AmnoonSa@users.noreply.github.com>
2024-08-12 16:36:00 +0300

    Add files via upload

--- src/js/js.js
+++ src/js/js.js
@@ -13,12 +13,18 @@ function scrollToSection(event) {
 
   const images = [
     "./src/img/Amnoon.png",
-    "./src/img/news1.jpg", // Replace with actual image paths
+    "./src/img/news1.jpg",
+    "./src/img/news2.jpg",
+    "./src/img/news3.png",
+    "./src/img/news4.png",
     // Add more image paths as needed
 ];
 const texts = [
-    "يقدم مكتب آمنون الاستشارات في المجال الأمني والرقمي للقطاع العالم والصناعي بمعايير عالمية و بما يتوافق مع متطلبات االهيئة العليا للأمن الصناعي من خلال نخبة متميزة من الخبرات والكفائات الوطنية بما يتوافق مع رؤية المملكة 2030",
-    "مكتب امنون للاستشارات الأمنية يشارك في الندوة الثالثة لأمن وسلامة المنشآت الحيوية بورقتي عمل يقدمها نخبة من المستشاريين الأمنيين ، الندوة المزمع إقامتها  في محافظة ظفار في سلطنة عُمان خلال الفترة من 2-4 سبتمبر",
+    "يقدم مكتب آمنون الاستشارات في المجال الأمني والرقمي للقطاع العام والصناعي بمعايير عالمية و بما يتوافق مع متطلبات الهيئة العليا للأمن الصناعي من خلال نخبة متميزة من الخبرات والكفائات الوطنية بما يتوافق مع رؤية المملكة 2030",
+    "مكتب امنون للاستشارات الأمنية يشارك في الندوة الثالثة لأمن وسلامة المنشآت الحيوية بورقتي عمل يقدمها نخبة من المستشاريين الأمنيين في الندوة المزمع إقامتها بمحافظة ظفار في سلطنة عُمان خلال الفترة من 2-4 سبتمبر",
+    "سوف يتشرف مكتب آمنون للاستشارات الأمنية بالمشاركة في ندوة (أمن المرافق الحيوية الثالثه ) والتي سوف تعقد في سلطنة عمان-صلالة - تحت عنوان (أمن الموانئ والمرافق الحيوية ) بورقة عمل (وظيفة الامن الرقمي في حماية بينات المرافق الحيوية) يقدمها المستشار الأمني لواء متقاعد دكتور بركة بن زامل الحوشان",
+    "يتشرف مكتب آمنون للاستشارات الأمنية بالمشاركة بورقة عمل في ندوة (أمن المرافق الحيوية) والتي سوف تعقد في سلطنة عمان-صلالة - تحت عنوان (أمن الموانئ والمرافق الحيوية ) بورقة عمل عن امن المطارات يقدمها المستشار الامني لواء متقاعد يوسف بن احمد القحطاني",
+    "سوف يتشرف مكتب آمنون للاستشارات الأمنية بالمشاركة في ندوة (أمن المرافق الحيوية الثالثة ) والتي سوف تعقد في سلطنة عمان-صلالة - تحت عنوان (أمن الموانئ والمرافق الحيوية ) بورقة عمل عن امن المواني يقدمها المستشار البحري عميد بحري متقاعد عبدالله بن ناصر اباالخيل",
     // Add more texts corresponding to the images
 ];
 
@@ -50,13 +56,19 @@ setInterval(changeContent3, 10000);
 
 const imagesEn = [
   "./src/img/Amnoon.png",
-  "./src/img/news1.jpg", // Replace with actual image paths
+  "./src/img/news1.jpg",
+  "./src/img/news2.jpg",
+  "./src/img/news3.png",
+  "./src/img/news4.png",
   // Add more image paths as needed
 ];
 const textsEn = [
-  "The Office of Security provides security and digital advice to the global and industrial sector with global standards and in accordance with the requirements of the Supreme Industrial Security Authority through a distinct selection of national expertise and competence in line with the Kingdom vision of 2030",
-  "Amnoon Security Consulting Office will participate in the Third Seminar on Security and Safety of Vital Installations with two papers presented by a distinguished team of security consultants. The seminar is scheduled to take place in Dhofar Governorate, Sultanate of Oman, from September 2nd to September 4th.",
-  // Add more texts corresponding to the images
+    "Amnon Security Consulting Office provides consulting in the fields of security and digital security for the public and industrial sectors with global standards and in accordance with the requirements of the Supreme Authority for Industrial Security. This is achieved through a distinguished group of national experts in alignment with the Kingdom's Vision 2030.",
+    "Amnon Security Consulting Office participates in the third seminar on the security and safety of critical infrastructure, presenting two working papers delivered by a selection of security consultants. The seminar will be held in Dhofar Governorate, Sultanate of Oman, from September 2-4.",
+    "Amnon Security Consulting Office is honored to participate in the third seminar on 'Critical Infrastructure Security,' which will be held in Salalah, Sultanate of Oman, under the title 'Port and Maritime Facility Security.' We will present a working paper on 'The Role of Digital Security in Protecting Critical Infrastructure Data,' delivered by retired Brigadier General Dr. Barakah bin Zamil Al-Hoshan",
+    "Amnon Security Consulting Office is honored to present a working paper at the 'Critical Infrastructure Security' seminar, which will be held in Salalah, Sultanate of Oman, under the title 'Port and Maritime Facility Security.' The paper on airport security will be presented by retired Brigadier General Yusuf bin Ahmed Al-Qahtani",
+    "Amnon Security Consulting Office is honored to participate in the third seminar on 'Critical Infrastructure Security,' which will be held in Salalah, Sultanate of Oman, under the title 'Port and Maritime Facility Security.' We will present a working paper on port security, delivered by retired Naval Admiral Abdullah bin Nasser Abaalkhail",
+    // Add more texts corresponding to the images
 ];
 
 let indexEn = 0;
